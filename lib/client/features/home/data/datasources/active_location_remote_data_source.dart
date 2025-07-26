7dc779e4d33880f67a91f0369ee4176731a80921
mohamed fahmy <109977202+mohamedfahmy1318@users.noreply.github.com>
2025-07-26 15:31:13 +0300

    feat: Refactor API endpoint handling and improve code structure in data sources

--- lib/client/features/home/data/datasources/active_location_remote_data_source.dart
+++ lib/client/features/home/data/datasources/active_location_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:x_go/core/constants/endpoints_strings.dart';
 import 'package:x_go/core/data/api/api_consumer.dart';
 
 import '../models/location_model.dart';
@@ -6,14 +7,15 @@ abstract class ActiveLocationRemoteDataSource {
   Future<ActiveLocationResponse> getActiveLocation();
 }
 
-class ActiveLocationRemoteDataSourceImpl implements ActiveLocationRemoteDataSource {
+class ActiveLocationRemoteDataSourceImpl
+    implements ActiveLocationRemoteDataSource {
   final ApiConsumer apiConsumer;
 
   ActiveLocationRemoteDataSourceImpl({required this.apiConsumer});
 
   @override
   Future<ActiveLocationResponse> getActiveLocation() async {
-    final response = await apiConsumer.get('active-locations');
+    final response = await apiConsumer.get(EndpointsStrings.activeLocation);
     return ActiveLocationResponse.fromJson(response);
   }
 }
